03ebb2492cfaab36b598b3119b0f9da016be5c7f
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-08 20:58:56 +0000

    version 1.0
    git-svn-id: http://plugins.svn.wordpress.org/proper-network-activation/trunk@261669 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ proper-network-activation.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: Proper Network Activation
+Version: 1.0
+Description: Use the network activation feature of WP MultiSite without problems
+Author: scribu
+Author URI: http://scribu.net/
+Plugin URI: http://scribu.net/wordpress/proper-network-activation
+
+
+Copyright (C) 2010 Cristi Burcă (scribu@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+( at your option ) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class Proper_Network_Activation {
+
+	static function init() {
+		add_action( 'activated_plugin',  array( __CLASS__, 'action' ) );
+		add_action( 'deactivated_plugin',  array( __CLASS__, 'action' ) );
+
+		add_action('wpmu_new_blog', array(__CLASS__, 'setup'));
+	}
+
+	static function action( $plugin ) {
+		if ( !is_plugin_active_for_network( $plugin ) )
+			return;
+
+		list( $action ) = explode( '_', current_filter(), 2 );
+
+		$action = str_replace( 'activated', 'activate', $action );
+
+		foreach ( self::get_active_blogs() as $blog_id ) {
+			switch_to_blog( $blog_id );
+
+			if ( in_array( $plugin, (array) get_option( 'active_plugins' ) ) )
+				continue;
+
+			self::do_action( $action, $plugin );
+		}
+
+		restore_current_blog();
+	}
+
+	static function setup( $blog_id ) {
+		switch_to_blog( $blog_id );
+
+		foreach ( array_keys( get_site_option( 'active_sitewide_plugins' ) ) as $plugin )
+			self::do_action( 'activate', $plugin );
+
+		restore_current_blog();
+	}
+
+	private static function do_action( $action, $plugin ) {
+		do_action( $action . '_' . $plugin );
+		do_action( $action . '_plugin', $plugin );
+	}
+
+	private static function get_active_blogs() {
+		global $wpdb;
+
+		return $wpdb->get_col( "
+			SELECT blog_id
+			FROM $wpdb->blogs
+			WHERE site_id = '{$wpdb->siteid}'
+			AND deleted = 0
+		" );
+	}
+}
+
+Proper_Network_Activation::init();
+
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Proper Network Activation ===
+Contributors: scribu
+Donate link: http://scribu.net/paypal
+Tags: activation, admin, plugins, multisite
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: trunk
+
+Avoid errors when using WP MU network activation
+
+== Description ==
+
+When running WordPress MultiSite, you have a very handy feature called network activation. It allows you to activate a plugin for the entire network of sites. The trouble is that it only does half the job.
+
+Some plugins have an install procedure that is meant to be run only on activation. However, when you do a network activation, that install procedure is only run for the current site. So, you end up with plugins not working properly on all the other sites.
+
+What this plugin does:
+
+* when doing a network de/activation, it triggers the de/activation hook on all sites in the network
+* when creating a new site, it triggers the activation hook for all active network plugins on that site
+
+**Note:** this plugin is not recommended for very large networks of sites.
+
+== Installation ==
+
+You can either install it automatically from the WordPress admin, or do it manually:
+
+1. Unzip "Front-end Editor" archive and put the folder into your plugins folder (/wp-content/plugins/).
+1. Activate the plugin from the Plugins menu.
+
+== Frequently Asked Questions ==
+
+= Error on activation: "Parse error: syntax error, unexpected..." =
+
+Make sure your host is running PHP 5. The only foolproof way to do this is to add this line to wp-config.php (after the opening `<?php` tag):
+
+`var_dump(PHP_VERSION);`
+<br>
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+* [more info](http://scribu.net/wordpress/proper-network-activation/pna-1-0.html)
+
