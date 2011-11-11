4efab12c1b2dc64326187ccd9eab273da383b4fc
scribu <mail@scribu.net>
2011-11-11 16:32:34 +0200

    update email address

--- proper-network-activation.php
+++ proper-network-activation.php
@@ -10,7 +10,7 @@ Network: true
 Text Domain: proper-network-activation
 Domain Path: /lang
 
-Copyright (C) 2010 Cristi Burcă (scribu@gmail.com)
+Copyright (C) 2010 Cristi Burcă (mail@scribu.net)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
